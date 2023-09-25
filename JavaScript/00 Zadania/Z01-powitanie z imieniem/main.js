9aa4d2850d8266acfb314e140c707888afc7599a
2p01 <boberxdbober@gmail.com>
2023-09-25 13:24:12 +0200

    oj2

---
+++ JavaScript/00 Zadania/Z01-powitanie z imieniem/main.js
@@ -0,0 +1,4 @@
+let imie = prompt("Podaj swoje imię: ","Jan")
+console.log(imie);
+document.write("<h1>Cześć "+imie+ "!!!!!!!!!!!!!!</h1>");
+document.write("<h2>Miło cię widzieć na naszej stronie.</h2>")
---
+++ JavaScript/00 Zadania/z02-srednia 4 liczb/main.js
@@ -0,0 +1,19 @@
+let a = prompt("Podaj a = ","");
+a = parseInt(a);
+let b = prompt("Podaj b = ","");
+b = parseInt(b);
+let c = prompt("Podaj c = ","");
+c = parseInt(c);
+let d = prompt("Podaj d = ","");
+d = parseInt(d);
+
+let suma = a+b+c+d;
+let srednia = suma/4;
+
+console.log("a = "+a);
+console.log("b = "+b);
+console.log("c = "+c);
+console.log("d = "+d);
+console.log("srednia = "+srednia);
+document.write("srednia = "+srednia);
+document.write(suma);
