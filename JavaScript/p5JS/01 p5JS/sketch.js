57d21122e35418566276fbb3f0581151e0a4eb37
2p01 <boberxdbober@gmail.com>
2023-11-13 13:24:26 +0100

    oj2

--- JavaScript/p5JS/01 p5JS/sketch.js
+++ JavaScript/p5JS/01 p5JS/sketch.js
@@ -1,13 +1,20 @@
 function setup() {
     createCanvas(900,700)
     background(150)
+    for (let i = 0; i < 5; i++){
+        let x1 = random(width)
+        let y1 = random(height)
+        let promien = random(50, 100)
+        let kolor = fill(Math.random() * 255 + 1, Math.floor(Math.random() * 255 + 1, Math.floor(Math.random() * 255 + 1)))
+        let szybkoscx = random(-5, 5)
+        let szybkoscy = random(-5, 5)
+        circles.push({x1 , y1, promien, kolor, szybkoscx, szybkoscy})
+        }
+
 }
 let x = 100;
 let y = 100;
 function draw() {
     background(150)
-    point(x++,y++)
-    ellipse(x++,random(200,300),50,150)
-    ellipse(mouseX,mouseY,30,40)
-
+    ellipse(x,y,50,50)
 }
